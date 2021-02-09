81bf04a9b03dbf58f42f37821d9d208df8633146
Panlol36 <75273854+Panlol36@users.noreply.github.com>
2021-02-09 20:19:02 +0100

    radio

--- radio.ino
+++ radio.ino
@@ -1,6 +1,7 @@
 #include <radio.h>
 #include <si4703.h>
 #include <Wire.h>
+#include <RDSParser.h>
 // nastavení propojovacích pinů
 
 #include <LiquidCrystal_I2C.h> 
@@ -8,11 +9,6 @@
 #define resetPin 2
 #define SDIO A4
 #define SCLK A5
-
-// proměnné pro běh programu
-char rdsBuffer[10];
-int  frekvence;
-
 
 // enkoder
 #define pinCLK 3
@@ -34,10 +30,14 @@ int stavPred2;
 int stavCLK2;
 int stavSW2;
 
+long lastButtonPressed = 0;
 unsigned long lastRefresh;
+bool backlightState;
+
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 SI4703 radio;
+RDSParser rds;
 
 void setup() {
     // Initiate the LCD:
@@ -49,6 +49,7 @@ void setup() {
 
   radio.setVolume(8);
   radio.setBandFrequency(RADIO_BAND_FM, 9650);
+  radio.debugEnable();
 
  // enkoder
  Serial.begin(9600);
@@ -72,11 +73,14 @@ void setup() {
   // načtení aktuálního stavu pinu CLK pro porovnávání
   stavPred2 = digitalRead(pinCLK2);  
 
+  radio.attachReceiveRDS(RDS_process);
+
+  rds.attachServicenNameCallback(DisplayServiceName);
 }
 
 void loop() 
 {
-char buffer[12];
+char buffer[14];
 
 
 radio.formatFrequency(buffer, sizeof(buffer));
@@ -92,47 +96,42 @@ if(millis()-lastRefresh > 100)
 {
   lastRefresh = millis();
   zobrazInfo(buffer, volume);
-  zobrazInfo(buffer, frekvence);
+
 }
 
+radio.checkRDS();
 
 }
 void zobrazInfo(char *frekvence, int hlasitost) {
   // vytištění informací z proměnných
+ 
   lcd.setCursor(0,0);
   lcd.print(frekvence);
+
+  lcd.setCursor(14,1);
+  lcd.print(hlasitost);
   lcd.print(" ");
-  lcd.setCursor(0,1);
-  lcd.print("Hlasitost: "); 
-  //if (hlasitost < 10)
-  //{
-    lcd.print(hlasitost);
-    lcd.print(" ");
- 
-    
-  //}
-  /*//else
-  {
-    lcd.print(hlasitost);
-  }*/
 }
 
 void checkEncoder(int hlasitost) 
 {
+    
     stavCLK = digitalRead(pinCLK);
+   
+
   if (stavCLK != stavPred) 
   {
       // pokud stav pinu DT neodpovídá stavu pinu CLK,
       // byl pin CLK změněn jako první a rotace byla
       // po směru hodin, tedy vpravo
       if (digitalRead(pinDT) != stavCLK) 
-      {
+      {  
         // vytištění zprávy o směru rotace a přičtení
         // hodnoty 1 u počítadla pozice enkodéru
     
         hlasitost ++;
         if (hlasitost > 15) 
-          hlasitost = 15;
+        hlasitost = 15;
 
           radio.setVolume(hlasitost);
       }
@@ -147,36 +146,82 @@ void checkEncoder(int hlasitost)
 
         radio.setVolume(hlasitost);
 
-         
-
       }  
   }
 stavPred = stavCLK;
-}
-void checkEncoder2(int frekvence) {
 
+}
+
+void checkEncoder2(char *frekvence) 
+{
+   
    stavCLK2 = digitalRead(pinCLK2);
  if (stavCLK2 != stavPred2){
    if (digitalRead(pinDT2) != stavCLK2)
    {
-     frekvence +=5;
-     if (frekvence > 9650)
-       frekvence = 9650;
-  radio.setBandFrequency(RADIO_BAND_FM, 9650);
+     frekvence += 10;
+     if (frekvence > 12000)
+       frekvence = 12000;
+  radio.setFrequency(frekvence);
 }
  else{
-     frekvence -=5;
+     frekvence -= 10;
      if (frekvence < 0)
      frekvence = 0;
      
-radio.setBandFrequency(RADIO_BAND_FM, 9650);
+radio.setFrequency(frekvence);
  }
    
 
  }
-     
-
-
+    
 stavPred2 = stavCLK2;
 
+stavSW2 = digitalRead(pinSW2);
+if (stavSW2 == 0){
+radio.seekDown();
+
 }
+stavSW = digitalRead(pinSW);
+if (stavSW == 0){
+radio.seekUp();
+}
+
+}
+
+  
+ void switchBacklight()
+{
+  if (backlightState == true){                         //check to see state of backlight
+    backlightState = false;                            //flip state of backlight
+  }
+  else if (backlightState == false){
+    backlightState = true;
+  }
+
+  if (backlightState == true){                         //the bool argument for backlight state (true/false)
+    lcd.setBacklight(HIGH);                            //the statement that actualy changes the backlight state
+  }
+  else if (backlightState == false){
+    lcd.setBacklight(LOW);
+  }
+}
+
+void DisplayServiceName(char *name)
+{
+  lcd.setCursor(0, 1);  
+  lcd.print(name);
+  Serial.print(name);
+}
+
+ // DisplayServiceName()
+
+void RDS_process(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t block4) {
+  rds.processData(block1, block2, block3, block4);
+}
+
+
+
+  
+  
+  
