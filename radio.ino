ed909376d0d0f314bbca3df991e8ffeb2a3560fc
vitoo420 <72879242+vitoo420@users.noreply.github.com>
2021-04-13 20:50:52 +0200

    verze k odevzdani

--- radio.ino
+++ radio.ino
@@ -4,7 +4,7 @@
 #include <RDSParser.h>
 // nastavení propojovacích pinů
 
-#include <LiquidCrystal_I2C.h> 
+#include <LiquidCrystal_I2C.h>
 
 #define resetPin 2
 #define SDIO A4
@@ -21,7 +21,9 @@
 #define pinSW2 9
 
 // rele
-#define pinRele 10
+#define pinReleRadio 10
+#define pinReleBluetooth 11
+#define pinReleGND 12
 // proměnné pro uložení pozice a stavů pro určení směru
 // a stavu tlačítka
 int stavPred;
@@ -33,18 +35,19 @@ int stavCLK2;
 int stavSW2;
 
 long lastButtonPressed = 0;
-unsigned long lastRefresh;
+unsigned long lastRefresh = 0;
+unsigned long lastEncoderCheck = 0;
 bool backlightState;
 
 bool bluetooth = false;
-
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 SI4703 radio;
 RDSParser rds;
 
-void setup() {
-    // Initiate the LCD:
+void setup()
+{
+  // Initiate the LCD:
   lcd.begin();
   lcd.backlight();
 
@@ -55,8 +58,12 @@ void setup() {
   radio.setBandFrequency(RADIO_BAND_FM, 9650);
   radio.debugEnable();
 
- // enkoder
- Serial.begin(9600);
+  // enkoder
+  Serial.begin(9600);
+
+  pinMode(pinReleRadio, OUTPUT);
+  pinMode(pinReleBluetooth, OUTPUT);
+  pinMode(pinReleGND, OUTPUT);
   // nastavení propojovacích pinů jako vstupních
   pinMode(pinCLK, INPUT);
   pinMode(pinDT, INPUT);
@@ -64,196 +71,267 @@ void setup() {
   // jako vstupní s pull up odporem
   pinMode(pinSW, INPUT_PULLUP);
   // načtení aktuálního stavu pinu CLK pro porovnávání
-  stavPred = digitalRead(pinCLK);   
+  stavPred = digitalRead(pinCLK);
 
-
- // enkoder
-   // nastavení propojovacích pinů jako vstupních
+  // enkoder
+  // nastavení propojovacích pinů jako vstupních
   pinMode(pinCLK2, INPUT);
   pinMode(pinDT2, INPUT);
   // nastavení propojovacího pinu pro tlačítko
   // jako vstupní s pull up odporem
   pinMode(pinSW2, INPUT_PULLUP);
   // načtení aktuálního stavu pinu CLK pro porovnávání
-  stavPred2 = digitalRead(pinCLK2);  
+  stavPred2 = digitalRead(pinCLK2);
 
   radio.attachReceiveRDS(RDS_process);
 
   rds.attachServicenNameCallback(DisplayServiceName);
+
+  digitalWrite(pinReleRadio, LOW);
+  digitalWrite(pinReleBluetooth, HIGH);
+  digitalWrite(pinReleGND, HIGH);
 }
 
-void loop() 
+void loop()
 {
-char buffer[14];
+  char buffer[14];
 
+  radio.formatFrequency(buffer, sizeof(buffer));
+  int volume = radio.getVolume();
+  int frekvence = radio.getFrequency();
 
-radio.formatFrequency(buffer, sizeof(buffer));
-int volume = radio.getVolume();
-int frekvence = radio.getFrequency();
+  checkEncoder(volume);
+  checkEncoder2(frekvence);
 
-
-checkEncoder(volume);
-checkEncoder2(frekvence);
-
-Serial.println(bluetooth);
-
-if(millis()-lastRefresh > 100)
-{
-  lastRefresh = millis();
-  zobrazInfo(buffer, volume);
-
-}
-
-radio.checkRDS();
-
-}
-void zobrazInfo(char *frekvence, int hlasitost) {
-  // vytištění informací z proměnných
-  lcd.setCursor(0, 0);
-  lcd.clear();
-  if(!bluetooth)
+  if (millis() - lastRefresh > 100)
   {
-  lcd.print(frekvence);
-  lcd.setCursor(14,1);
-  lcd.print(hlasitost);
-  lcd.print(" ");
+    lastRefresh = millis();
+    zobrazInfo(buffer, volume);
+  }
+
+  radio.checkRDS();
+}
+void zobrazInfo(char *frekvence, int hlasitost)
+{
+  // vytištění informací z proměnných
+
+  lcd.clear();
+  if (!bluetooth)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print(frekvence);
+    lcd.setCursor(14, 1);
+    lcd.print(hlasitost);
+    lcd.print(" ");
   }
   else
   {
-    lcd.setCursor(3,0);
+    lcd.setCursor(3, 0);
     lcd.print("BLUETOOTH");
   }
-  
 }
 
-void checkEncoder(int hlasitost) 
+void checkEncoder(int hlasitost)
 {
-    
-    stavCLK = digitalRead(pinCLK);
-   
 
-  if (stavCLK != stavPred) 
+  stavCLK = digitalRead(pinCLK);
+
+  if (stavCLK != stavPred)
   {
-      // pokud stav pinu DT neodpovídá stavu pinu CLK,
-      // byl pin CLK změněn jako první a rotace byla
-      // po směru hodin, tedy vpravo
-      if (digitalRead(pinDT) != stavCLK) 
-      {  
-        // vytištění zprávy o směru rotace a přičtení
-        // hodnoty 1 u počítadla pozice enkodéru
-    
-        hlasitost ++;
-        if (hlasitost > 15) 
+    // pokud stav pinu DT neodpovídá stavu pinu CLK,
+    // byl pin CLK změněn jako první a rotace byla
+    // po směru hodin, tedy vpravo
+    if (digitalRead(pinDT) != stavCLK)
+    {
+      // vytištění zprávy o směru rotace a přičtení
+      // hodnoty 1 u počítadla pozice enkodéru
+
+      hlasitost++;
+      if (hlasitost >= 15)
         hlasitost = 15;
 
-          radio.setVolume(hlasitost);
-      }
-      else 
-      {
-        // vytištění zprávy o směru rotace a odečtení
-        // hodnoty 1 u počítadla pozice enkodéru
-        
-        hlasitost --;  
-        if (hlasitost < 0) 
+      radio.setVolume(hlasitost);
+    }
+    else
+    {
+      // vytištění zprávy o směru rotace a odečtení
+      // hodnoty 1 u počítadla pozice enkodéru
+
+      hlasitost--;
+      if (hlasitost <= 0)
         hlasitost = 0;
 
-        radio.setVolume(hlasitost);
-
-      }  
+      radio.setVolume(hlasitost);
+    }
   }
-stavPred = stavCLK;
-
+  stavPred = stavCLK;
 }
 
-void checkEncoder2(char *frekvence) 
+void checkEncoder2(char *frekvence)
 {
-   
-   stavCLK2 = digitalRead(pinCLK2);
- if (stavCLK2 != stavPred2){
-   if (digitalRead(pinDT2) != stavCLK2)
-   {
-     frekvence += 10;
-     if (frekvence > 12000)
-       frekvence = 12000;
-  radio.setFrequency(frekvence);
-}
- else{
-     frekvence -= 10;
-     if (frekvence < 0)
-     frekvence = 0;
-     
-radio.setFrequency(frekvence);
- }
-   
 
- }
-    
-stavPred2 = stavCLK2;
+  stavCLK2 = digitalRead(pinCLK2);
+  if (stavCLK2 != stavPred2)
+  {
+    if (digitalRead(pinDT2) != stavCLK2)
+    {
+      frekvence += 10;
+      if (frekvence > 12000)
+        frekvence = 12000;
+      radio.setFrequency(frekvence);
+    }
+    else
+    {
+      frekvence -= 10;
+      if (frekvence < 0)
+        frekvence = 0;
 
-/*stavSW2 = digitalRead(pinSW2);
-if (stavSW2 == 0){
-radio.seekDown();
+      radio.setFrequency(frekvence);
+    }
+  }
 
-}
-stavSW = digitalRead(pinSW);
-if (stavSW == 0){
-radio.seekUp();
+  stavPred2 = stavCLK2;
+
+  stavSW2 = digitalRead(pinSW2);
+  if (stavSW2 == 0)
+  {
+    radio.seekDown();
+  }
+  stavSW = digitalRead(pinSW);
+  if (stavSW == 0)
+  {
+    radio.seekUp();
+  }
+
+  /*if (millis() - lastEncoderCheck > 250)
+{
+  lastEncoderCheck = millis();
+  stavSW2 = digitalRead(pinSW2);
+  if (stavSW2 == 0)
+  {
+    if (bluetooth == false)
+    {
+      digitalWrite(pinReleRadio, HIGH);
+      digitalWrite(pinReleBluetooth, LOW);
+      digitalWrite(pinReleGND, LOW);
+
+      bluetooth = true;
+    }
+    else
+    {
+      digitalWrite(pinReleRadio, LOW);
+      digitalWrite(pinReleBluetooth, HIGH);
+      digitalWrite(pinReleGND, HIGH);
+      
+      bluetooth = false;
+    }
+  }
 }*/
 
-stavSW2 = digitalRead(pinSW2);
-if (stavSW2 == 0)
-{
-  if(digitalRead(pinRele) == LOW)
+  stavSW = digitalRead(pinSW);
+  if (stavSW == 0)
   {
-    digitalWrite(pinRele, HIGH);
-    bluetooth = true;
+    radio.seekUp();
   }
-  else
-  {
-    digitalWrite(pinRele,LOW);
-    bluetooth = false;
-  }
-}
-stavSW = digitalRead(pinSW);
-if (stavSW == 0)
-{
-  radio.seekUp();
-}
 }
 
-  
- void switchBacklight()
+void switchBacklight()
 {
-  if (backlightState == true){                         //check to see state of backlight
-    backlightState = false;                            //flip state of backlight
+  if (backlightState == true)
+  {                         //check to see state of backlight
+    backlightState = false; //flip state of backlight
   }
-  else if (backlightState == false){
+  else if (backlightState == false)
+  {
     backlightState = true;
   }
 
-  if (backlightState == true){                         //the bool argument for backlight state (true/false)
-    lcd.setBacklight(HIGH);                            //the statement that actualy changes the backlight state
+  if (backlightState == true)
+  {                         //the bool argument for backlight state (true/false)
+    lcd.setBacklight(HIGH); //the statement that actualy changes the backlight state
   }
-  else if (backlightState == false){
+  else if (backlightState == false)
+  {
     lcd.setBacklight(LOW);
   }
 }
 
 void DisplayServiceName(char *name)
 {
-  lcd.setCursor(0, 1);  
+  lcd.setCursor(0, 1);
   lcd.print(name);
   Serial.print(name);
 }
 
- // DisplayServiceName()
+// DisplayServiceName()
 
-void RDS_process(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t block4) {
+void RDS_process(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t block4)
+{
   rds.processData(block1, block2, block3, block4);
 }
 
+/*class Encoder
+{
+  private:
+    byte CLT;
+    byte DT;
+    byte SW;
+    unsigned long lastPressTime = 0;
+    unsigned long pressDelay = 50;
 
+  public:
+  Led(byte CLT, byte DT, byte SW)
+  {
+    // Use 'this->' to make the difference between the
+    // 'pin' attribute of the class and the
+    // local variable 'pin' created from the parameter.
+    this->CLT = CLT;
+    this->DT = DT;
+    this->SW = SW;
+    init();
+  }
 
-  
-  
-  
+  void init()
+  {
+    pinMode(CLK, INPUT);
+    pinMode(DT, INPUT);
+    // nastavení propojovacího pinu pro tlačítko
+    // jako vstupní s pull up odporem
+    pinMode(SW, INPUT_PULLUP);
+    update();
+  }
+  void update()
+  {
+    stavCLK = digitalRead(CLK);
+
+    if (stavCLK != stavPred)
+    {
+      // pokud stav pinu DT neodpovídá stavu pinu CLK,
+      // byl pin CLK změněn jako první a rotace byla
+      // po směru hodin, tedy vpravo
+      if (digitalRead(DT) != stavCLK)
+      {
+        // vytištění zprávy o směru rotace a přičtení
+        // hodnoty 1 u počítadla pozice enkodéru
+
+        hlasitost++;
+        if (hlasitost >= 15)
+          hlasitost = 15;
+
+        radio.setVolume(hlasitost);
+      }
+      else
+      {
+        // vytištění zprávy o směru rotace a odečtení
+        // hodnoty 1 u počítadla pozice enkodéru
+
+        hlasitost--;
+        if (hlasitost <= 0)
+          hlasitost = 0;
+
+        radio.setVolume(hlasitost);
+      }
+    }
+    stavPred = stavCLK;
+  }
+}*/
